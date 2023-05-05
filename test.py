a97a1c689245b75fbab87d7a885ea20ece35b947
Owen Busler <Owbusler@amazon.com>
2023-05-05 15:50:49 -0700

    init

---
+++ test.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('foothillHoffman.osm')
+root = tree.getroot()
+
+nodes = {} #node ID to list [] of wayIDS which contain it
+streets = []
+validStreetTypes = ['residential', 'tertiary']
+
+for item in root:
+	if(item.tag == 'node'):
+		nodes[item.attrib['id']] = []
+
+
+print(len(nodes))
+for item in root:
+	if(item.tag == 'way'):
+		for nd in item:
+			if(nd.tag =='nd'):
+				#print(nd.attrib['ref'])
+				if(nodes[nd.attrib['ref']] == None):
+					nodes[nd.attrib['ref']] = [1]
+				nodes[nd.attrib['ref']].append(item.attrib['id'])
+		
+			if(nd.tag == 'tag' and nd.attrib['k'] == 'highway' and nd.attrib['v'] in validStreetTypes ):
+				streets.append(item.attrib['id'])
+
+
+
+for node in nodes:
+	if(len(nodes[node]) > 0):
+		#print(nodes[node])
+		pass
+
+print(streets)
